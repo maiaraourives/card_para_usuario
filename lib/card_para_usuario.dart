1c0997618db61ab3481eef29e78a3b01a8d6ce6a
Maiara Ferreira <maifesil@gmail.com>
2023-07-17 08:28:44 -0300

    Inicio

--- lib/card_para_usuario.dart
+++ lib/card_para_usuario.dart
@@ -1,7 +1,93 @@
 library card_para_usuario;
 
-/// A Calculator.
-class Calculator {
-  /// Returns [value] plus 1.
-  int addOne(int value) => value + 1;
+import 'package:flutter/material.dart';
+
+class CardParaUsuario extends StatelessWidget {
+  const CardParaUsuario({
+    required this.descricacaoUnidade,
+    required this.nome,
+    this.textColor,
+    this.onSelect,
+    this.future,
+    this.hasSelected = false,
+    super.key,
+  });
+
+  final Function()? onSelect;
+  final Future<Object?>? future;
+  final bool hasSelected;
+  final String nome;
+  final String descricacaoUnidade;
+  final Color? textColor;
+
+  @override
+  Widget build(BuildContext context) {
+    final theme = Theme.of(context);
+
+    return Card(
+      elevation: 5,
+      margin: const EdgeInsets.symmetric(horizontal: 10).copyWith(top: 10),
+      shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(15)),
+      child: InkWell(
+        onTap: onSelect,
+        borderRadius: BorderRadius.circular(15),
+        splashColor: theme.colorScheme.onPrimary,
+        focusColor: theme.colorScheme.onPrimary,
+        hoverColor: theme.colorScheme.onPrimary,
+        highlightColor: theme.colorScheme.onPrimary,
+        child: Padding(
+          padding: const EdgeInsets.all(10),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Row(
+                mainAxisSize: MainAxisSize.min,
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  CircleAvatar(
+                    radius: 30,
+                    backgroundColor: Colors.transparent,
+                    child: ClipOval(
+                      child: Image.network(
+                        'https://cdn-icons-png.flaticon.com/512/1/1247.png',
+                        fit: BoxFit.fill,
+                        width: double.infinity,
+                        height: double.infinity,
+                      ),
+                    ),
+                  ),
+                  const SizedBox(width: 10),
+                  Expanded(
+                    child: Column(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        Text(
+                          nome,
+                          style: TextStyle(
+                            color: Colors.grey[600] ?? textColor,
+                            fontWeight: FontWeight.w600,
+                            fontSize: 17,
+                          ),
+                        ),
+                        const SizedBox(height: 2),
+                        Text(
+                          descricacaoUnidade,
+                          style: TextStyle(
+                            color: Colors.grey[600] ?? textColor,
+                            fontSize: 15,
+                          ),
+                        ),
+                      ],
+                    ),
+                  ),
+                ],
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
 }
